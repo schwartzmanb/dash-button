491e7ff69cd8c7c6fb6d83f839f30f1892a40b38
schwartzmanb <schwartzman.ben@gmail.com>
2018-10-01 16:33:56 -0400

    Add optional handling for packet listening vs. importing known buttons and detecting them.

--- src/dashbutton/dashButton.py
+++ src/dashbutton/dashButton.py
@@ -1,28 +1,53 @@
 from scapy.all import sniff
 import csv
+import sys
 
 buttonDict = {}
 
-def readCSV():
-	for key, val in csv.reader(open("buttons.csv", 'r')):
-		buttonDict[key] = val
-	
+# Open csv file from path specified
+
+
+def readCSV(filename):
+    try:
+        csvFile = open(filename, 'r')
+    except IOError:
+        print("File does not exist.")
+        return 1
+    for key, val in csv.reader(csvFile):
+        buttonDict[key] = val
+
+# Return key of button (name) upon detection of MAC address
+
+
 def button_detect(pkt):
-	pktKey = "ARP"
-	if pkt[pktKey].op == 1:
-		for i in buttonDict:
-			if pkt[pktKey].hwsrc == buttonDict[i]:
-				return buttonDict[i]
+    pktKey = "ARP"
+    if pkt[pktKey].op == 1:
+        for i in buttonDict:
+            if pkt[pktKey].hwsrc == buttonDict[i]:
+                return buttonDict[i]
 
-#readCSV()
 
-#print(sniff(prn=button_detect, filter="arp", store=0))
+def arp_detect(pkt):
+    pktKey = "ARP"
+    if pkt[pktKey].op == 1:
+        return pkt[pktKey].hwsrc
+
 
 def main():
-	readCSV()
-	for key in buttonDict:
-		print(key + " " + buttonDict[key])
-	print(sniff(prn=button_detect, filter="arp", store=0))
+    if "--listen" in sys.argv:
+        print(sniff(prn=arp_detect, filter="arp", store=0))
+    elif "-l" in sys.argv:
+        print(sniff(prn=arp_detect, filter="arp", store=0))
+    else:
+    	for key in buttonDict:
+        	print(key + " " + buttonDict[key])
+    	print(sniff(prn=button_detect, filter="arp", store=0))
+
 
 if __name__ == "__main__":
-	main()
+    if "--read" in sys.argv:
+        readCSV(sys.argv[sys.argv.index("--read") + 1])
+    elif "-r" in sys.argv:
+        readCSV(sys.argv[sys.argv.index("-r") + 1])
+
+    main()
